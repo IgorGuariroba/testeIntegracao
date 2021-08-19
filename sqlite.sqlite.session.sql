c54606ef268010dbbc3bbdb903c1d5dc63e32e1c
IgorGuariroba <1g0r.guari@gmail.com>
2021-08-19 00:40:16 -0300

    Excluindo os dados do banco de forma manual no banco

---
+++ sqlite.sqlite.session.sql
@@ -0,0 +1,1 @@
+DELETE FROM leiloes WHERE true;
--- tests/Integration/Dao/LeilaoDaoTest.php
+++ tests/Integration/Dao/LeilaoDaoTest.php
@@ -11,17 +11,24 @@ class LeilaoDaoTest extends TestCase
 {
     public function testInsercaoEBuscaDevemFuncionar()
     {
+        // Arrange
         $leilao = new Leilao('Variante 0KM');
-        $leilaoDao = new DaoLeilao(ConnectionCreator::getConnection());
-
+        $pdo = ConnectionCreator::getConnection();
+        $leilaoDao = new DaoLeilao($pdo);
         $leilaoDao->salva($leilao);
+
+        // act
         $leiloes = $leilaoDao->recuperarNaoFinalizados();
 
+        // assert 
         self::assertCount(1, $leiloes);
         self::assertContainsOnlyInstancesOf(Leilao::class, $leiloes);
         self::assertSame(
-            'Viariante 0Km',
+            'Variante 0KM',
             $leiloes[0]->recuperarDescricao()
         );
+
+        // tear down
+        $pdo->exec('DELETE FROM leiloes WHERE TRUE;');
     }
 }
